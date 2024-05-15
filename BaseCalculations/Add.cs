ec7dc9136fcdc6ee642e8a4b335a3be390ab03a5
jayan4537 <Jayanta.Dutta@towerswatson.com>
2024-05-15 20:14:09 +0530

    Add project files.

---
+++ BaseCalculations/Add.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp.BaseCalculations
+{
+    public class Add
+    {
+        public  int a = 8;
+        public int add(int x, int y)
+        {
+            var result = x + y;
+            return result;
+        }
+        public int add(int x, int y, int z)
+        {
+            var result = x + y + z;
+            return result;
+        }
+        public int add(List<int> list)
+        {
+            int result = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                result = result + list[i];
+            }
+            return result;
+        }
+        public void test()
+        {
+
+        }
+
+    }
+    public class two:Add
+    {
+        public int add(int x, int y)
+        {
+            var result = x + y;
+            return result;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using TestConsoleApp.BaseCalculations;
+
+static void main()
+{
+    two obj = new two();
+    Add obj2 = new two();
+    obj.test();
+    obj.add(1, 2);
+    obj2.add(1, 2);
+}
+main();
